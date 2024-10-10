0082a7967780bc1f02f4c92a5deea57a7d6b0605
maciejmar <memgos@gmail.com>
2024-10-10 15:31:59 +0200

    test add user email changed only to rerun test

--- goods-django/goods_django/goods_django_mongodb/test_add_user_req.py
+++ goods-django/goods_django/goods_django_mongodb/test_add_user_req.py
@@ -4,7 +4,7 @@ url = 'http://localhost:8000/add_user/'
 data = {
     "username": "marek",
     "password": "drecki",
-    "email": "mdrecki@webaby.io"
+    "email": "mdreckiecki@webaby.io"
 }
 
 response = requests.post(url, json=data)
